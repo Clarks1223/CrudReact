ef61d37cc4db6da1c29a865edfc982bd4f994355
Clarks1223 <pablo.uchuari@epn.edu.ec>
2024-05-06 19:54:54 -0500

    CRUD con BD Json terminado

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,212 @@
+import Mensajes from './Mensajes';
+import { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+export const Formulario = ({ setEstado, idMetro }) => {
+  const [error, setError] = useState(false);
+  const [mensaje, setMensaje] = useState(false);
+  const [form, setForm] = useState({
+    nombre: '',
+    sector: '',
+    salida: '',
+    llegada: '',
+    maquinista: '',
+    detalles: '',
+  });
+  //Efecto que se ejecuta cuando se da click en actualizar una ruta, en el componente listar
+  useEffect(() => {
+    if (idMetro) {
+      (async function (idMetro) {
+        try {
+          const respuesta = await (
+            await fetch(`http://localhost:3000/metro/${idMetro}`)
+          ).json();
+          const { id, nombre, sector, salida, llegada, maquinista, detalles } =
+            respuesta;
+          setForm({
+            ...form,
+            nombre,
+            sector,
+            salida,
+            llegada,
+            maquinista,
+            detalles,
+            id,
+          });
+          console.log('ha llegado un elemento para actualizar: ', form);
+        } catch (error) {
+          console.log(error);
+        }
+      })(idMetro);
+    }
+  }, [idMetro]);
+  //evento que actua cuando se preciona click en registrar ruta
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (Object.values(form).includes('') || Object.entries(form).length == 0) {
+      setError(true);
+      setTimeout(() => {
+        setError(false);
+      }, 1000);
+      return;
+    }
+    try {
+      if (form.id) {
+        const URL = `http://localhost:3000/metro/${form.id}`;
+        await fetch(URL, {
+          method: 'PUT',
+          body: JSON.stringify(form),
+          headers: { 'Content-Type': 'application/json' },
+        });
+        setEstado(true);
+        setForm({});
+        setTimeout(() => {
+          setEstado(false);
+          setForm({});
+        }, 1000);
+      } else {
+        //se declara la url del API
+        const URL = 'http://localhost:3000/metro';
+        //se asigna un identificador unico a cada formulario
+        form.id = uuidv4();
+        //solicitud a la url con fetch
+        await fetch(URL, {
+          //se especifica el tipo de solicitud
+          method: 'POST',
+          //se convierte el objeto form en un objeto tipo JSON para hacerlo el cuerpo de la solicitud
+          body: JSON.stringify(form),
+          // establece los encabezados de la solicitud
+          headers: { 'Content-Type': 'application/json' },
+        });
+        setMensaje(true);
+        setEstado(true);
+        setTimeout(() => {
+          setMensaje(false);
+          setEstado(false);
+          setForm({});
+        }, 1000);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleChange = (e) => {
+    // actualiza el valor del estado form, con los que ingresa el usuario
+    setForm({ ...form, [e.target.name]: e.target.value.trim() });
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <Mensajes tipo={'bg-red-900'}>validar campos</Mensajes>
+      <div>
+        <label
+          htmlFor="nombre"
+          className="text-gray-700 uppercase font-bold text-sm"
+        >
+          Nombre:{' '}
+        </label>
+        <input
+          id="nombre"
+          type="text"
+          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5"
+          placeholder="Nombre de la ruta"
+          name="nombre"
+          value={form.nombre || ''}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="sector"
+          className="text-gray-700 uppercase font-bold text-sm"
+        >
+          Sector:{' '}
+        </label>
+        <input
+          id="sector"
+          type="text"
+          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5"
+          placeholder="Sector de la ruta"
+          name="sector"
+          value={form.sector || ''}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="salida"
+          className="text-gray-700 uppercase font-bold text-sm"
+        >
+          Punto de salida:{' '}
+        </label>
+        <input
+          id="salida"
+          type="text"
+          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5"
+          placeholder="Punto de salida"
+          name="salida"
+          value={form.salida || ''}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="llegada"
+          className="text-gray-700 uppercase font-bold text-sm"
+        >
+          Punto de llegada:{' '}
+        </label>
+        <input
+          id="llegada"
+          type="text"
+          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5"
+          placeholder="Punto de llegada"
+          name="llegada"
+          value={form.llegada || ''}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="maquinista"
+          className="text-gray-700 uppercase font-bold text-sm"
+        >
+          Nombre del maquinista:{' '}
+        </label>
+        <input
+          id="maquinista"
+          type="text"
+          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5"
+          placeholder="Nombre del maquinista"
+          name="maquinista"
+          value={form.maquinista || ''}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="detalles"
+          className="text-gray-700 uppercase font-bold text-sm"
+        >
+          Detalles:{' '}
+        </label>
+        <textarea
+          id="detalles"
+          type="text"
+          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5"
+          name="detalles"
+          value={form.detalles || ''}
+          onChange={handleChange}
+        />
+      </div>
+      {error && <Mensajes tipo="bg-red-900">"Existen campos vacios"</Mensajes>}
+      {mensaje && <Mensajes tipo="bg-green-900">"Registro existoso"</Mensajes>}
+      <input
+        type="submit"
+        className="bg-sky-900 w-full p-3
+        text-white uppercase font-bold rounded-lg
+        hover:bg-red-900 cursor-pointer transition-all"
+        value={form.id ? 'Actualizar ruta' : 'Registrar ruta'}
+      />
+    </form>
+  );
+};
---
+++ src/components/Listar.jsx
@@ -0,0 +1,102 @@
+import logoMetro from '../assets/tren.webp';
+import Mensajes from './Mensajes';
+import { useState, useEffect } from 'react';
+
+const Listar = ({ estado, setIdMetro }) => {
+  //Declaro los estados
+  const [rutas, setRutas] = useState([]);
+  const [eliminado, setEliminado] = useState(false);
+  //Efecto para cargar todos los datos de la BD
+  useEffect(() => {
+    if (estado || rutas.length >= 0) {
+      (async function () {
+        try {
+          const respuesta = await (
+            await fetch('http://localhost:3000/metro')
+          ).json();
+          setRutas(respuesta);
+          console.log('Lo que llega', rutas);
+        } catch (error) {
+          console.log(error);
+        }
+      })();
+    }
+  }, [estado, eliminado]);
+  // eliminar una ruta
+  const handleDelete = async (id) => {
+    try {
+      //alerta al usuario si desea eliminar las rutas
+      const confirmar = confirm(
+        'Quieres eliminar la ruta, esta accion no se puede revertir'
+      );
+      //si la confirmacion es positiva
+      if (confirmar) {
+        //realiza el fetch con el metodo delte
+        const URL = `http://localhost:3000/metro/${id}`;
+
+        await fetch(URL, { method: 'DELETE' });
+        //crea un nuevo arreglo con las rutas y saca la ruta que se elimino
+        const nuevasRutas = rutas.filter((ruta) => ruta.id !== id);
+        //guarda las rutas
+        setRutas(nuevasRutas);
+        //actualiza las rutas
+        alert('Ruta eliminada');
+        setEliminado(!eliminado);
+      }
+    } catch (error) {
+      console.log('No se ha eliminado la ruta');
+    }
+  };
+  return (
+    <>
+      {rutas.length === 0 ? (
+        <Mensajes tipo={'bg-green-900'}>"No existe rutas creadas"</Mensajes>
+      ) : (
+        rutas.map((ruta) => (
+          <div
+            key={ruta.id}
+            className="p-2 rounded-xl sm:flex gap-12 bg-gray-200 shadow-xl mb-5"
+          >
+            <img
+              src={logoMetro}
+              alt="art cover"
+              className="sm:w-3/12 object-cover rounded-lg"
+            />
+
+            <div className="h-auto p-3 w-full">
+              <h4 className="text-2xl font-semibold text-cyan-900">
+                {ruta.nombre}
+              </h4>
+              <hr className="w-full border border-gray-300 my-2" />
+              <p className="text-gray-500">Sector: {ruta.sector}</p>
+              <p className="text-gray-500">Punto de salida: {ruta.salida}</p>
+              <p className="text-gray-500">Punto de llegada: {ruta.llegada}</p>
+              <p className="text-gray-500">Maquinista: {ruta.maquinista}</p>
+              <p className="text-gray-500">Detalles: {ruta.detalles}</p>
+              <div className="flex justify-between mt-3 lg:justify-end md:justify-end gap-3">
+                <button
+                  className="bg-sky-900 text-white px-6 py-1 rounded-full"
+                  onClick={() => {
+                    setIdMetro(ruta.id);
+                  }}
+                >
+                  Actualizar
+                </button>
+                <button
+                  className="bg-red-900 text-white px-6 py-1 rounded-full"
+                  onClick={() => {
+                    handleDelete(ruta.id);
+                  }}
+                >
+                  Eliminar
+                </button>
+              </div>
+            </div>
+          </div>
+        ))
+      )}
+    </>
+  );
+};
+
+export default Listar;
